a261d37f322d99092383f87c2cfd78c296a0b98b
afiovinicius <afiovinicius@gmail.com>
2025-04-27 01:06:21 -0300

    feat: add requirements

--- src/app/main.py
+++ src/app/main.py
@@ -111,7 +111,7 @@ async def home(short: str, db: Session = Depends(get_db)):
         )
 
 
-if __name__ == "__main__":
-    import uvicorn
+# if __name__ == "__main__":
+#     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+#     uvicorn.run(app, host="0.0.0.0", port=8080)
